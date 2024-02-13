b10c67199ac4b5d1ab5aa21c5e88de4ed5938e92
zehrakovar97@gmail.com <zehrakovar97@gmail.com>
2024-02-13 21:53:11 +0300

    Add Repository

---
+++ Application/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<List<T>> GetAllAsycc();
+        Task<T> GetByIdAsync(Guid id);
+        Task CreateAsync(T entity);
+        Task Update(T entity);
+        Task Remove(T entity);
+    }
+}
---
+++ Persistance/Repositories/Repository.cs
@@ -0,0 +1,47 @@
+﻿using Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Persistance.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        public readonly OnionContext _context;
+        public Repository(OnionContext context)
+        {
+            _context = context;
+        }
+        public async Task CreateAsync(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<T>> GetAllAsycc()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task Remove(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
