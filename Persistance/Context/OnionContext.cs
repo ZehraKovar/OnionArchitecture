869fc6a5202208ab2dad391ea16e74ddd03fd7b0
zehrakovar97@gmail.com <zehrakovar97@gmail.com>
2024-02-13 13:41:10 +0300

    add domainLayer

---
+++ Persistance/Context/OnionContext.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance.Context
+{
+    public class OnionContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var conString= "Host=localhost;Port=5432;Database=OnionArchitecture;Username=postgres;Password=qawsedrf";
+            optionsBuilder.UseNpgsql(conString);
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
