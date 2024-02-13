8ff023abd4a9b5a1b3343792b965ee6d6b0a798b
zehrakovar97@gmail.com <zehrakovar97@gmail.com>
2024-02-13 22:24:35 +0300

    Add CQRS pattern

---
+++ Application/Features/CQRS/Book/AddBook/AddBookCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.CQRS.Book.AddBook
+{
+    public class AddBookCommand
+    {
+        public string Name {  get; set; }
+    }
+}
---
+++ Application/Features/CQRS/Book/AddBook/AddBookHandler.cs
@@ -0,0 +1,27 @@
+﻿using Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.CQRS.Book.AddBook
+{
+    public class AddBookHandler
+    {
+        private readonly IRepository<Domain.Entities.Book> _repository;
+        public AddBookHandler(IRepository<Domain.Entities.Book> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handler(AddBookCommand command)
+        {
+            await _repository.CreateAsync(new Domain.Entities.Book
+            {
+                Name=command.Name,
+                Id=Guid.NewGuid()
+            });
+        }
+    }
+}
